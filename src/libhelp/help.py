ae84225b2bdb2a213a134ea168f5c5726b49088d
webro-13 <robotmen13@gmail.com>
2025-02-20 13:30:31 -0500

    ver0.0.7

--- src/libhelp/help.py
+++ src/libhelp/help.py
@@ -1,9 +1,9 @@
 print('this is a python helper library made to help you figure out the syntax of other python libraries. just type the name of the library you want to know about in the parentheses, and it will tell you the syntax of the functions in that library. to find out what libraries I know, type known libs in the parentheses.')
 
 def hlib(lib):
-    if lib == 'known libs':
+    if lib.lower() == 'known libs':
         print('pycheet')
-    elif lib == 'pycheet':
+    elif lib.lower() == 'pycheet':
         print('pycheet is a python library that has a few functions that print out different greetings.')
         print('to use the functions in this library, type "from pycheet import hello"')
         print('then type "hello.th()" to print "Hello, World!"')
