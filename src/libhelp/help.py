bedee8c6abd0633a936073f6cda5dd96701f9fad
webro-13 <robotmen13@gmail.com>
2025-02-25 14:49:37 -0500

    ver0.0.8

--- src/libhelp/help.py
+++ src/libhelp/help.py
@@ -4,9 +4,14 @@ def hlib(lib):
     if lib.lower() == 'known libs':
         print('pycheet')
     elif lib.lower() == 'pycheet':
-        print('pycheet is a python library that has a few functions that print out different greetings.')
+        print('pycheet is a python library that has a few functions; and allows you to "cheet" on your code. to use the functions in this library, type "from pycheet import hello"')
         print('to use the functions in this library, type "from pycheet import hello"')
         print('then type "hello.th()" to print "Hello, World!"')
         print('type "hello.h()" to print "Hello"')
         print('type "hello.hn(name)" to print "Hello " + name')
         print('type "hello.sh()" to print "Hi"')
+        print('and for the password function, type "from pycheet import password"')
+        print('then type "password.pass(password)" store a password')
+        print('then you can use password.get_pass() to get the password')
+    else:
+        print('I do not know that library/function. try typing known libs to find out what libraries I know.')
